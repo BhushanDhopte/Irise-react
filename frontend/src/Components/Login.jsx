55f9a41132099481f4dd2fa39535fc8cca6625c3
BhushanDhopte <144302290+BhushanDhopte@users.noreply.github.com>
2023-11-08 10:28:29 +0530

    update

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,19 +1,57 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 
 function Login(){
-
+    const [loginData, setLoginData]=useState({email:"",password:""})
     const router = useNavigate();
+    console.log(loginData, "loginData state")
+
+    function handleChange(event){
+        setLoginData({...loginData,[event.target.name]:event.target.value})
+    }
 
     function gotoRegister(){
         router('/register')
     }
 
+    async function Login(event){
+        event.preventDefault();
+        if (loginData.email && loginData.password) {
+            if (loginData.password.length > 7) {
+                try {
+                    // const response = await axios.post('http://localhost:8000/login', { loginData })
+                    const response = { data: { success: true, message: "Login successfull." } }
+                    if (response.data.success) {
+                        alert(response.data.message)
+                        setLoginData({ email: "", password: "" })
+                        router('/')
+                    }
+                } catch (error) {
+                    alert(error.response.data.message)
+                }
+            } else {
+                alert("Password length must be more than 7 digit.")
+            }
+        } else {
+            alert("All fields are mandatory.")
+        }
+    }
+
     return (
         <div>
             <h1>Login</h1>
+            <form onSubmit={Login}>
+                <label>Email</label><br />
+                <input type="email" onChange={handleChange} name="email" value={loginData.email} /><br />
+                <label>Password</label><br />
+                <input type="password" onChange={handleChange} name="password" value={loginData.password} /><br />
+                <input type="submit" value="Login" /><br/>
+                <label>Don't have a account    </label>
         <button onClick={gotoRegister}>Register</button>
+            </form>
+           
         </div>
     )
 }
